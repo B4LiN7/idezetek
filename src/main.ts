1182bf4be5618864ec7a1daaf5e10c11981efefe
B4LiN7 <balint.k.furedi@outlook.com>
2023-11-06 15:15:37 +0100

    F3

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,76 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { quotes } from "./quotes.json";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const content: HTMLElement = document.getElementById("content") as HTMLElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.addEventListener("DOMContentLoaded", () => { 
+
+  // 1. Feladat: ABC szerint rendezés
+  document.getElementById("f1")?.addEventListener("click", () => {
+    const quotesAbc = Array.from(quotes);
+    quotesAbc.sort((a, b) => a.quote.localeCompare(b.quote));
+
+    const ul = document.createElement("ul");
+
+    quotesAbc.forEach((quote) => {
+      const li = document.createElement("li");
+      li.innerText = quote.author + ": ";
+
+      const quoteLi = document.createElement("q");
+      quoteLi.innerText = quote.quote;
+
+      li.appendChild(quoteLi);
+      ul.appendChild(li);
+    });
+
+    content.innerHTML = "";
+    content.appendChild(ul);
+  });
+
+  // 2. Felaadat: The kiemelése
+  document.getElementById("f2")?.addEventListener("click", () => { 
+    let quotesThe: string[] = [];
+
+    quotes.forEach((quote) => {
+        quotesThe.push(quote.quote);
+    });
+
+    const ol = document.createElement("ol");
+
+    quotesThe.forEach((quote) => {
+      const li = document.createElement("li");
+      li.innerHTML = quote.replace("the", "<b>the</b>").replace("The", "<b>The</b>");
+      ol.appendChild(li);
+
+    });
+
+    content.innerHTML = "";
+    content.appendChild(ol);
+
+  });
+
+  // 3. Felaadat: Hosszak kiírása
+  document.getElementById("f3")?.addEventListener("click", () => { 
+    let quotesLenght: number[] = [];
+
+    quotes.forEach((quote) => {
+        quotesLenght.push(quote.quote.length);
+    });
+
+    const p = document.createElement("p");
+
+    p.innerText = quotesLenght.join(", ");
+
+    content.innerHTML = "";
+    content.appendChild(p);
+
+  });
+
+  // 4. Felaadat: Darabszám
+  document.getElementById("f4")?.addEventListener("click", () => { 
+    
+
+
+  });
+
+
+});
